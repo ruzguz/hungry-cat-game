b7e7f7856740854d8fe562f522e4f4301635452b
Paulo Puleo <paulopuleo95@gmail.com>
2020-08-17 19:04:36 -0400

    Se agrega la funcion de escalar

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class ObstacleController : MonoBehaviour
 {
-    //Los objetos deberian tener un peso con respecto al gato, asi el gato solo mueve objetos si tiene la fuerza necesaria
-    //Paso a seguir
-        //Bloquear el movimiento y rotacion del objecto en todos sus ejes
-        //Cuando el jugador esta en contacto con el trigger verifica la fuerza del gato
-        //Si la fuerza del gato es igual o mayor al peso del objecto
-        //entonces desbloquea su movimiento en el eje X
-        //si el jugador no esta en contacto entonces bloquea su movimieto en X
+    //-Los objetos deberian tener un peso con respecto al gato, asi el gato solo mueve objetos si tiene la fuerza necesaria
+    //-Paso a seguir
+        //-Bloquear el movimiento y rotacion del objecto en todos sus ejes
+        //-Cuando el jugador esta en contacto con el trigger verifica la fuerza del gato
+        //-Si la fuerza del gato es igual o mayor al peso del objecto
+        //-entonces desbloquea su movimiento en el eje X
+        //-si el jugador no esta en contacto entonces bloquea su movimieto en X
 
     public int weight; //peso 1 para gato gordo, peso 2 para gato normal, peso 3 para gato fit
 
@@ -18,7 +18,7 @@ public class ObstacleController : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D Objeto)
     {
 
-        //Verificar si el Objecto es gato y la fuerza es suficiente
+        //-Verificar si el Objecto es gato y la fuerza es suficiente
         if (Objeto.gameObject.tag == "Player" && Objeto.gameObject.GetComponent<PlayerController>().force >= weight)
         {
             GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
@@ -27,7 +27,7 @@ public class ObstacleController : MonoBehaviour
     }
     private void OnCollisionExit2D(Collision2D Objeto)
     {
-        //Verificar si el Objecto es gato y la fuerza es suficiente, si la fuerza no era suficiente significa que no estaba empujando
+        //-Verificar si el Objecto es gato y la fuerza es suficiente, si la fuerza no era suficiente significa que no estaba empujando
         if (Objeto.gameObject.tag == "Player" && Objeto.gameObject.GetComponent<PlayerController>().force >= weight)
         {
             GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
