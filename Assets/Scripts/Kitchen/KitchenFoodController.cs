2772591d63ceaa643bb4eda2e3e18617706165b4
Rusben Guzman <rusbenguzman@gmail.com>
2020-09-02 06:22:32 -0400

    copy FoodController code into KitchenFoodController.cs

--- Assets/Scripts/Kitchen/KitchenFoodController.cs
+++ Assets/Scripts/Kitchen/KitchenFoodController.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class KitchenFoodController : MonoBehaviour
 {
+   // Components vars
+    SpriteRenderer _foodSR;
+
+    // Food vars
+    public FoodType foodType;
+
+
+    // Moving effets vars
+    Vector3 startPosition;
+    [SerializeField]
+    float _speed = 2f;
+    [SerializeField]
+    float _lineDistance = 0.5f;
+
+
+    /// Awake is called when the script instance is being loaded.
+    void Awake()
+    {
+        startPosition = this.transform.position;       
+        _foodSR = GetComponent<SpriteRenderer>(); 
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +35,14 @@ public class KitchenFoodController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Moving effect
+        //this.transform.position = new Vector3(transform.position.x, 
+        //                                      startPosition.y + Mathf.Sin(Time.time * _speed) * _lineDistance);
+    }
+
+    // Function to hide the game object
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
     }
 }
